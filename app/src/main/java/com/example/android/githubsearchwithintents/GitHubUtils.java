e43ca6e7168424846db42921dda1dc7691b2deac
Rob Hess <robwhess@gmail.com>
2018-02-06 13:48:50 -0800

    Bootstrap app with code from previous app.

---
+++ app/src/main/java/com/example/android/githubsearchwithintents/GitHubUtils.java
@@ -0,0 +1,56 @@
+package com.example.android.githubsearchwithintents;
+
+import android.net.Uri;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hessro on 1/30/18.
+ */
+
+public class GitHubUtils {
+    final static String GITHUB_SEARCH_BASE_URL = "https://api.github.com/search/repositories";
+    final static String GITHUB_SEARCH_QUERY_PARAM = "q";
+    final static String GITHUB_SEARCH_SORT_PARAM = "sort";
+    final static String GITHUB_SEARCH_SORT_VALUE = "stars";
+
+    public static class SearchResult {
+        public String fullName;
+        public String description;
+        public String htmlURL;
+        public int stars;
+    }
+
+    public static String buildGitHubSearchURL(String searchQuery) {
+        return Uri.parse(GITHUB_SEARCH_BASE_URL).buildUpon()
+                .appendQueryParameter(GITHUB_SEARCH_QUERY_PARAM, searchQuery)
+                .appendQueryParameter(GITHUB_SEARCH_SORT_PARAM, GITHUB_SEARCH_SORT_VALUE)
+                .build()
+                .toString();
+    }
+
+    public static ArrayList<SearchResult> parseSearchResultsJSON(String searchResultsJSON) {
+        try {
+            JSONObject searchResultsObj = new JSONObject(searchResultsJSON);
+            JSONArray searchResultsItems = searchResultsObj.getJSONArray("items");
+
+            ArrayList<SearchResult> searchResultsList = new ArrayList<SearchResult>();
+            for (int i = 0; i < searchResultsItems.length(); i++) {
+                SearchResult result = new SearchResult();
+                JSONObject resultItem = searchResultsItems.getJSONObject(i);
+                result.fullName = resultItem.getString("full_name");
+                result.description = resultItem.getString("description");
+                result.htmlURL = resultItem.getString("html_url");
+                result.stars = resultItem.getInt("stargazers_count");
+                searchResultsList.add(result);
+            }
+            return searchResultsList;
+        } catch (JSONException e) {
+            return null;
+        }
+    }
+}
